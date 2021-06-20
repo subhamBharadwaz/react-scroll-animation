219a736bea30276046af08e8004bb74600c9428c
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-06-20 22:53:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.scss";
+import useWindowSize from "./hooks/useWindowSize";
+import images from "./images/images";
 
 function App() {
+  // Hook
+  const size = useWindowSize();
+  // RFF
+  const app = useRef();
+  const scrollContainer = useRef();
+  const skewConfigs = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
+  useEffect(() => {
+    setBodyHeight();
+  }, [size.height]);
+
+  const setBodyHeight = () => {
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`;
+  };
+
+  const skewScrolling = () => {
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous +=
+      (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+
+    // variables
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    //
+    scrollContainer.current.style.transform = `translateY(-${skewConfigs.rounded}px) skewY(${skew}deg)`;
+
+    requestAnimationFrame(() => skewScrolling());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={app} className="App">
+      <div ref={scrollContainer} className="scroll">
+        {images.map((image, index) => (
+          <>
+            <div key={index} className="img-container">
+              <img src={image} alt={`people ${index}`} />
+            </div>
+            <h2>
+              React <span className="outline">Scrolling</span>
+            </h2>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/images/images.js
@@ -0,0 +1,10 @@
+const images = [
+  "https://images.pexels.com/photos/1085823/pexels-photo-1085823.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/2710131/pexels-photo-2710131.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.unsplash.com/photo-1593806812862-1dc510b769a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80",
+  "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+  "https://images.unsplash.com/photo-1484517186945-df8151a1a871?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80",
+  "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+];
+
+export default images;
